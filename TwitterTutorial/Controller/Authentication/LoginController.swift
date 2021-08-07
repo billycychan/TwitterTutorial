8966918fe5e5c6f1cf96ce21340223457396ad4e
Billy CY Chan <billy.cychan@lalamove.com>
2021-08-08 01:12:28 +0800

    feat: creating input container view

--- TwitterTutorial/Controller/Authentication/LoginController.swift
+++ TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -18,6 +18,38 @@ class LoginController: UIViewController {
         return imageView
     }()
     
+    private lazy var emailConatinerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        
+        view.addSubview(imageView)
+        imageView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor,
+                         paddingLeft: 8, paddingBottom: 8)
+        imageView.setDimensions(width: 24, height: 24)
+
+        return view
+    }()
+    
+    private lazy var passwordConatinerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPurple
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        
+        view.addSubview(imageView)
+        imageView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor,
+                         paddingLeft: 8, paddingBottom: 8)
+        imageView.setDimensions(width: 24, height: 24)
+
+        return view
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +67,13 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailConatinerView, passwordConatinerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
     }
 
 }
