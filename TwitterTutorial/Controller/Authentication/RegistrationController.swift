c6d253765c11f9d39e2418fabc92d4e7998a67a6
Billy CY Chan <billy.cychan@lalamove.com>
2021-08-08 01:03:38 +0800

    feat: add logoImageView

---
+++ TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,26 @@
+//
+//  RegistrationController.swift
+//  TwitterTutorial
+//
+//  Created by billy.cychan on 8/8/2021.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+
+}
