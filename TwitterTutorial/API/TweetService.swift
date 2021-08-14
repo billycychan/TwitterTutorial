19db71e80be68a0e73b6faf271a8bedaf7cf0ada
Billy CY Chan <billy.cychan@lalamove.com>
2021-08-14 18:45:19 +0800

    fix: key error

--- TwitterTutorial/API/TweetService.swift
+++ TwitterTutorial/API/TweetService.swift
@@ -17,8 +17,8 @@ struct TweetService {
         let values = ["uid": uid,
                       "timestamp": Int(NSDate().timeIntervalSince1970),
                       "likes": 0,
-                      "retweets:": 0,
-                      "caption:": caption] as [String: Any]
+                      "retweets": 0,
+                      "caption": caption] as [String: Any]
         
         REF_TWEETS.childByAutoId().updateChildValues(values, withCompletionBlock: completion)
     }
@@ -26,7 +26,7 @@ struct TweetService {
     func fetchTweets(completion: @escaping ([Tweet]) -> Void) {
         var tweets = [Tweet]()
         
-        REF_TWEETS.observe(.childAdded) { snapshot in            
+        REF_TWEETS.observe(.childAdded) { snapshot in
             guard let dictionary = snapshot.value as? [String: Any] else { return }
             let tweetID = snapshot.key
             let tweet = Tweet(tweetID: tweetID, dictionary: dictionary)
