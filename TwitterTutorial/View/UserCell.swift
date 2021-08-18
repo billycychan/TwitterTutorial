ca06f70061236c37594e053f8789fa94270030e5
Billy CY Chan <bill0930@gmail.com>
2021-08-19 00:58:07 +0800

    feat: follow-search, fetch and display users in explore controller

---
+++ TwitterTutorial/View/UserCell.swift
@@ -0,0 +1,73 @@
+//
+//  UserCell.swift
+//  TwitterTutorial
+//
+//  Created by billy.cychan on 19/8/2021.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    var user: User? {
+        didSet { configure() }
+    }
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.setDimensions(width: 40, height: 40)
+        imageView.layer.cornerRadius = 40 / 2
+        imageView.layer.masksToBounds = true
+        imageView.backgroundColor = .twitterBlue
+        return imageView
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+        guard let user = user else { return }
+        profileImageView.sd_setImage(with: user.profileImageUrl)
+        usernameLabel.text = user.username
+        fullnameLabel.text = user.fullname
+    }
+    
+}
